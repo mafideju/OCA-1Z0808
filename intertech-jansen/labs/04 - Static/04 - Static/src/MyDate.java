e8833e0ed54b4237d0ff0ab8c973049d8df33859
Marcio Rodrigues <mafideju@outlook.com>
2021-10-06 17:56:57 -0300

    feat(intertech): lab 04 statics

---
+++ intertech-jansen/labs/04 - Static/04 - Static/src/MyDate.java
@@ -0,0 +1,28 @@
+
+public class MyDate {
+
+	int dia, mes, ano;
+	
+	public MyDate() {
+		this.dia = 11;
+		this.mes = 9;
+		this.ano = 2001;
+	}
+	
+	public MyDate(int dia, int mes, int ano) {
+		setDate(dia, mes, ano);
+	}
+	
+	public void setDate(int dia, int mes, int ano) {
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	}
+
+	@Override
+	public String toString() {
+		return "MyDate [Hoje é dia = " + dia + ", mes = " + mes + ", do ano = " + ano + "]";
+	}
+	
+	
+}
