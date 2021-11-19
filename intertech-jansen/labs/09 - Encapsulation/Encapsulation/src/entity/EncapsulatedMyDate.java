73b2abb45b20c8d5aa7a9c88b9bbcd7ee256eb91
Marcio Rodrigues <mafideju@outlook.com>
2021-11-19 11:36:50 -0300

    feat(intertech): labs 09

---
+++ intertech-jansen/labs/09 - Encapsulation/Encapsulation/src/entity/EncapsulatedMyDate.java
@@ -0,0 +1,56 @@
+package entity;
+
+public class EncapsulatedMyDate {
+	
+
+	private int dia;
+	private int mes;
+	private int ano;
+	
+	public EncapsulatedMyDate() {
+		this.setDia(11);
+		this.setMes(9);
+		this.setAno(2001);
+	}
+	
+	public EncapsulatedMyDate(int dia, int mes, int ano) {
+		setDate(dia, mes, ano);
+	}
+	
+	public void setDate(int dia, int mes, int ano) {
+		this.setDia(dia);
+		this.setMes(mes);
+		this.setAno(ano);
+	}
+
+	@Override
+	public String toString() {
+		return "MyDate [Hoje é dia = " + getDia() + ", mes = " + getMes() + ", do ano = " + getAno() + "]";
+	}
+
+	public int getMes() {
+		return mes;
+	}
+
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+
+	public int getDia() {
+		return dia;
+	}
+
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	
+
+}
---
+++ intertech-jansen/labs/09 - Encapsulation/Encapsulation/src/tests/TestsEncapsulatedMyDate.java
@@ -0,0 +1,31 @@
+package tests;
+
+import entity.EncapsulatedMyDate;
+
+public class TestsEncapsulatedMyDate {
+	public static void main(String[] args) { 
+		
+		EncapsulatedMyDate date1 = new EncapsulatedMyDate(11,11,1918);
+		EncapsulatedMyDate date2 = new EncapsulatedMyDate();
+		
+		date2.setDia(02);
+		date2.setMes(05);
+		date2.setAno(1983);
+		
+		EncapsulatedMyDate date3 = new EncapsulatedMyDate();
+		date3.setDate(21,4,1988);
+		
+		EncapsulatedMyDate date4 = new EncapsulatedMyDate();
+		
+		String str1 = date1.toString();
+		String str2 = date2.toString();
+		String str3 = date3.toString();
+		String str4 = date4.toString();
+		
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+		System.out.println(str4);
+	}
+
+}
